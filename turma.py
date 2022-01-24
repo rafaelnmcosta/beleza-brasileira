672147a7aa1baa6b7db804f85e7f599595596f5b
rafaelnmcosta <rafaelnmcosta@outlook.com>
2022-01-24 20:50:34 -0300

    Exemplos adicionados

---
+++ turma.py
@@ -0,0 +1,15 @@
+class Turma:
+    
+    turma = 'Nome Qualquer'
+    
+    def __init__(self, paramTurma):
+        self.turma = paramTurma # variável de instância
+    
+    
+# tA = Turma('A') 
+# tB = Turma('B')
+# tC = Turma('C')
+# print(Turma.turma) #variável de classe
+# print(tA.turma)    #variável de instância
+# print(tB.turma)    #variável de instância
+# print(tC.turma)    #variável de instância
