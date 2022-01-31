3f34ec1199a1431a681f57704ddf278a7edaff1e
Evellyn Nicole <mevellyn4@gmail.com>
2022-01-31 17:30:09 -0300

    teste atoa

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ user = Usuario()
 user.cadastro()
 
 print("\nnome: ", user.nome, "\nendereco: ", user.endereco, "\ntelefone: ", user.telefone, "\nid: ", user.id, "\nsenha: ", user.senha, "\ntipo: ", user.tipo)
+print("teste")
 
 print(user.id)
 user.id = "novoId"
