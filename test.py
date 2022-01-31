7c8360d1e103c65475e363124f09674114fcf1c2
rafaelnmcosta <rafaelnmcosta@outlook.com>
2022-01-31 17:13:04 -0300

    teste

--- test.py
+++ test.py
@@ -5,3 +5,5 @@ user = Usuario()
 user.cadastro()
 
 print("\nnome: ", user.nome, "\nendereco: ", user.endereco, "\ntelefone: ", user.telefone, "\nid: ", user.id, "\nsenha: ", user.senha, "\ntipo: ", user.tipo)
+
+#teste
