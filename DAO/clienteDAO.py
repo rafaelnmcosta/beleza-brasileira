a30f8769bd62404d3c4d53b0052f0c0e81cb1b00
rafaelnmcosta <-l>
2022-03-28 17:40:19 -0300

    Correção na classe ClienteDAO

--- DAO/clienteDAO.py
+++ DAO/clienteDAO.py
@@ -1,35 +1,52 @@
 import sqlite3
-from cliente import Cliente
+from Model.cliente import Cliente
+
+class ClienteDAO:
+
+    def __init__(self, param_database):
+        self.__database = param_database #String que contém o nome do banco de dados que será acessado
+
+    @property
+    def database(self):
+        return self.__database
 
 
-# Função que abre a conexão com o banco de dados
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+    @database.setter
+    def ref(self, new_database):
+        raise ValueError("Nao e permitido alterar o banco de dados")
 
-# Função que recebe um objeto cliente e o adiciona ao banco de dados
-def addCliente(novo_cliente):
-    conn = get_db_connection()
-    conn.execute('INSERT INTO users'
-        '(nome, endereco, telefone, ref, senha, tipo, cpf_cnpj, descricao)'
-        'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-        (novo_cliente.nome, novo_cliente.endereco, novo_cliente.telefone, novo_cliente.ref, novo_cliente.senha, novo_cliente.tipo, novo_cliente.cpf, None)
-    )
-    conn.commit()
-    conn.close()
-    return True
 
-# Função que busca uma ref, instancia um objeto cliente com as informações e retorna esse objeto
-def getCliente(ref_cliente):
-    conn = get_db_connection()
-    cliente = list(conn.execute('SELECT * FROM users WHERE ref = ?', (ref_cliente,)).fetchone())
-    conn.close()
-    if cliente is None:
-        #abort(404)
-        return False
-    else:
-        cli = Cliente(
+    # Função que abre a conexão com o banco de dados
+    def get_db_connection(self):
+        conn = sqlite3.connect(self.__database)
+        if conn:
+            conn.row_factory = sqlite3.Row
+            return conn
+        else:
+            return False
+
+    # Função que recebe um objeto cliente e o adiciona ao banco de dados
+    def addCliente(self, novo_cliente):
+        conn = self.get_db_connection()
+        conn.execute('INSERT INTO users'
+            '(nome, endereco, telefone, ref, senha, tipo, cpf_cnpj, descricao)'
+            'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            (novo_cliente.nome, novo_cliente.endereco, novo_cliente.telefone, novo_cliente.ref, novo_cliente.senha, novo_cliente.tipo, novo_cliente.cpf, None)
+        )
+        conn.commit()
+        conn.close()
+        return True
+
+    # Função que busca uma ref, instancia um objeto cliente com as informações e retorna esse objeto
+    def getCliente(self, ref_cliente):
+        conn = self.get_db_connection()
+        cliente = list(conn.execute('SELECT * FROM users WHERE ref = ?', (ref_cliente,)).fetchone())
+        conn.close()
+        if cliente is None:
+            #abort(404)
+            return False
+        else:
+            cli = Cliente(
                 cliente[1],
                 cliente[2],
                 cliente[3],
@@ -37,14 +54,14 @@ def getCliente(ref_cliente):
                 cliente[5],
                 cliente[6],
                 cliente[7]
-                )
-        return cli
+            )   
+            return cli
 
-#Função que deleta um registro do banco de dados de acordo com a referencia informada
-def deleteCliente(ref_cliente):
-        cli_del = getCliente(ref_cliente)
-        if(cli_del):
-            conn = get_db_connection()
+    #Função que deleta um registro do banco de dados de acordo com a referencia informada
+    def deleteCliente(self, ref_cliente):
+        cli_del = self.getCliente(ref_cliente)
+        if cli_del:
+            conn = self.get_db_connection()
             conn.execute('DELETE FROM users WHERE ref = ?', (ref_cliente,))
             conn.commit()
             conn.close()
@@ -52,41 +69,41 @@ def deleteCliente(ref_cliente):
         else:
             return False
 
-#Função que instancia um objeto para cada cliente no banco de dados e os coloca em uma lista, depois retorna essa lista
-def listClientes():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM users WHERE tipo = "cliente"').fetchall()
-    conn.close()
-    clientes = []
-    for user in users:
-        user = list(user)
-        cli_atual = Cliente(
-                user[1],
-                user[2],
-                user[3],
-                user[4],
-                user[5],
-                user[6],
-                user[7]
-                )
-        clientes.append(cli_atual)
-    return clientes
+    #Função que instancia um objeto para cada cliente no banco de dados e os coloca em uma lista, depois retorna essa lista
+    def listClientes(self):
+        conn = self.get_db_connection()
+        clientes_info = conn.execute('SELECT * FROM users WHERE tipo = "cliente"').fetchall()
+        conn.close()
+        clientes = []
+        for cli in clientes_info:
+            cli = list(cli)
+            cli_atual = Cliente(
+                cli[1],
+                cli[2],
+                cli[3],
+                cli[4],
+                cli[5],
+                cli[6],
+                cli[7]
+            )
+            clientes.append(cli_atual)
+        return clientes
 
-#Função que recebe um objeto cliente e uma string ref, e altera no BD, na linha da ref,
-# os dados de acordo com as informações do objeto passado como parâmetro 
-def updateCliente(ref_original, cli_alterado):
-        conn = get_db_connection()
+    #Função que recebe um objeto cliente e uma string ref, e altera no BD, na linha da ref,
+    # os dados de acordo com as informações do objeto passado como parâmetro 
+    def updateCliente(self, ref_original, cli_alterado):
+        conn = self.get_db_connection()
         conn.execute('UPDATE users SET nome = ?, endereco = ?, telefone = ?, ref = ?, senha = ?, cpf_cnpj = ?'
-                        ' WHERE ref = ?',
-                        (cli_alterado.nome,
-                        cli_alterado.endereco,
-                        cli_alterado.telefone,
-                        cli_alterado.ref,
-                        cli_alterado.senha,
-                        cli_alterado.cpf,
-                        ref_original
-                        )
-                )
+            ' WHERE ref = ?',
+            (cli_alterado.nome,
+            cli_alterado.endereco,
+            cli_alterado.telefone,
+            cli_alterado.ref,
+            cli_alterado.senha,
+            cli_alterado.cpf,
+            ref_original
+            )
+        )
         conn.commit()
         conn.close()
         return True
--- teste.py
+++ teste.py
@@ -1,97 +1,13 @@
 import sqlite3
 from Model.cliente import Cliente
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def getCliente(ref_cliente):
-    conn = get_db_connection()
-    cliente = list(conn.execute('SELECT * FROM users WHERE ref = ?', (ref_cliente,)).fetchone())
-    conn.close()
-    if cliente is None:
-        #abort(404)
-        print('cliente nao existe')
-        return False
-    else:
-        cli = Cliente(
-                cliente[1],
-                cliente[2],
-                cliente[3],
-                cliente[4],
-                cliente[5],
-                cliente[6],
-                cliente[7]
-                )
-        return cli
-
-
-def listClientes():
-    conn = get_db_connection()
-    users = conn.execute('SELECT * FROM users WHERE tipo = "cliente"').fetchall()
-    conn.close()
-    clientes = []
-    for user in users:
-        user = list(user)
-        cli_atual = Cliente(
-                user[1],
-                user[2],
-                user[3],
-                user[4],
-                user[5],
-                user[6],
-                user[7]
-                )
-        clientes.append(cli_atual)
-    return clientes
-
-
-def deleteCliente(ref_cliente):
-        cli_del = getCliente(ref_cliente)
-        if(cli_del):
-                conn = get_db_connection()
-                conn.execute('DELETE FROM users WHERE ref = ?', (ref_cliente,))
-                conn.commit()
-                conn.close()
-                return cli_del.nome
-        else: return False
-
-
-def addCliente(novo_cliente):
-    conn = get_db_connection()
-    conn.execute('INSERT INTO users'
-        '(nome, endereco, telefone, ref, senha, tipo, cpf_cnpj, descricao)'
-        'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-        (novo_cliente.nome, novo_cliente.endereco, novo_cliente.telefone, novo_cliente.ref, novo_cliente.senha, novo_cliente.tipo, novo_cliente.cpf, None)
-    )
-    conn.commit()
-    conn.close()
-    return True
-
-
-def updateCliente(ref_original, cli_alterado):
-        conn = get_db_connection()
-        conn.execute('UPDATE users SET nome = ?, endereco = ?, telefone = ?, ref = ?, senha = ?, cpf_cnpj = ?'
-                        ' WHERE ref = ?',
-                        (cli_alterado.nome,
-                        cli_alterado.endereco,
-                        cli_alterado.telefone,
-                        cli_alterado.ref,
-                        cli_alterado.senha,
-                        cli_alterado.cpf,
-                        ref_original
-                        )
-                )
-        conn.commit()
-        conn.close()
-        return True
+from DAO.clienteDAO import ClienteDAO
 
 
 #---------------------------------------------------------------------------------------------------#
+novoDAO = ClienteDAO('database.db')
+
 print("\nLista de cliente no inicio:")
-listaCli = listClientes()
+listaCli = novoDAO.listClientes()
 for cli in listaCli:
         print(cli.nome)
         print(cli.cpf)
@@ -100,30 +16,30 @@ for cli in listaCli:
 novoCli2 = Cliente("Cliente 2", "endereco 2", "telefone 2", "cliente2", "12345", "cliente", "cpf-novo-2")
 novoCli3 = Cliente("Cliente 3", "endereco 3", "telefone 3", "cliente3", "12345", "cliente", "cpf-novo-3")
 
-addCliente(novoCli2)
-addCliente(novoCli3)
+novoDAO.addCliente(novoCli2)
+novoDAO.addCliente(novoCli3)
 
 print("\nlista com clientes adicionados")
-listaCli = listClientes()
+listaCli = novoDAO.listClientes()
 for cli in listaCli:
         print(cli.nome)
         print(cli.cpf)
 
 cli_editado = Cliente("Cliente 3 v2", "endereco 3 v2", "telefone 3 v2", novoCli3.ref, "12345", "cliente", "cpf-novo-3 v2")
-updateCliente(novoCli3.ref, cli_editado)
+novoDAO.updateCliente(novoCli3.ref, cli_editado)
 
 print("\nlista com o cliente alterado:")
-listaCli = listClientes()
+listaCli = novoDAO.listClientes()
 for cli in listaCli:
         print(cli.nome)
         print(cli.cpf)
 
 print("\n")
-print("O ", deleteCliente(novoCli2.ref), " foi deletado")
-print("O ", deleteCliente(novoCli3.ref), " foi deletado")
+print("O ", novoDAO.deleteCliente(novoCli2.ref), " foi deletado")
+print("O ", novoDAO.deleteCliente(novoCli3.ref), " foi deletado")
 
 print("\nLista no fim do programa")
-listaCli = listClientes()
+listaCli = novoDAO.listClientes()
 for cli in listaCli:
         print(cli.nome)
         print(cli.cpf)
