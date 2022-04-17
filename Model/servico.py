373900c714d7e01e871a00ad57ad28451d0c55bf
Alerhandra Magalhães <89949656+alerhandramg@users.noreply.github.com>
2022-04-17 20:30:37 -0300

    Correção no modelo de serviços

--- Model/servico.py
+++ Model/servico.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 class Servico:
 
-    def __init__(self, param_data, param_horario, param_duracao, param_descricao, param_cliente, param_estab):
+    def _init_(self, param_data, param_horario, param_descricao, param_cliente, param_estab):
         self.__data = param_data #datetime.strptime(paramData, '%d/%m/%Y')
         self.__horario = param_horario #datetime.strptime(paramHorario, '%H:%M')
-        self.__duracao = param_duracao
         self.__descricao = param_descricao
         self.__ref_cliente = param_cliente # Referencia do usuário que está requisitando um serviço
         self.__ref_estab = param_estab # Referencia do estabelecimento que realizará o serviço
@@ -17,10 +16,6 @@ class Servico:
     @property
     def horario(self):
         return self.__horario
-
-    @property
-    def duracao(self):
-        return self.__duracao
 
     @property
     def descricao(self):
@@ -42,10 +37,6 @@ class Servico:
     def horario(self, new_horario):
         raise ValueError("Impossivel alterar o horario diretamente. Use a funcao de novo servico/edicao.")
 
-    @duracao.setter
-    def duracao(self, new_duracao):
-        raise ValueError("Impossivel alterar a duracao diretamente. Use a funcao de novo servico/edicao.")
-
     @descricao.setter
     def descricao(self, new_descricao):
         raise ValueError("Impossivel alterar a descricao diretamente. Use a funcao de novo servico/edicao.")
@@ -57,6 +48,3 @@ class Servico:
     @estabelecimento.setter
     def estab(self, new_estab):
         raise ValueError("Impossivel alterar o estabelecimento diretamente. Use a funcao de novo servico/edicao.")
-
-    def novo_servico(self):
-        print("Novo serviço\n")
